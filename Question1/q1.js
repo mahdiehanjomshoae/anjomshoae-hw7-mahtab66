767d58147dfc607496ff74d64d66038070612a55
mahdieh <mahdieh.anjomshoae98@gmail.com>
2021-12-22 23:48:54 +0330

    add complete q1

--- Question1/q1.js
+++ Question1/q1.js
@@ -1,35 +1,23 @@
-function Person1(ClassName, classUnit, ClassCapacity) {
-    this.ClassName = ClassName;
-    this.classUnit = classUnit;
-    this.ClassCapacity = ClassCapacity;
+const Person = function(ClassName, classUnit, ClassCapacity,book,proj){
+   this.ClassName = ClassName;
+   this.classUnit = classUnit;
+   this.ClassCapacity = ClassCapacity;
+   this.addMybook = function(){
+     this.book = book; 
+     }
+     this.addMyproj = function(){
+       this.proj = proj; 
+       }
+     
 }
-function networkClass1 (project){
-   super(ClassName, classUnit, ClassCapacity)
-   this.project=project
-   
-}
-function softEngineeringClass1 (book){
-   this.book=book
-}
-const networkClass2 = new networkClass1("network", "3", 30,"true");
-   const softEngineeringClass2 = new softEngineeringClass1("softEngieering", "3", "50","bookman");
-  console.log(`{ClassName :${networkClass2.ClassName}, classUnit:${networkClass2.classUnit}, ClassCapacity:${networkClass2.ClassCapacity} ,project:${networkClass2.project} }`)
-  console.log(`{ClassName :${softEngineeringClass2.ClassName}, classUnit:${softEngineeringClass2.classUnit}, ClassCapacity:${softEngineeringClass2.ClassCapacity} ,book:${softEngineeringClass2.book} }`)
-//   class networkClass extends Person1{
-//      constructor(ClassName, classUnit, ClassCapacity,project){
-//         super(ClassName, classUnit, ClassCapacity )
-//         this.project=project 
-//      }
-//  }
-//  class softEngineeringClass extends Person1{
-//     constructor(ClassName, classUnit, ClassCapacity,book){
-//        super(ClassName, classUnit, ClassCapacity )
-//        this.book=book 
-//     }
-// }
-  const softEngineeringClas = new Person1("softEngieering", "3", "50");
-  const networkClas = new Person1("network", "3", 30);
-  console.log (`{ClassName :${softEngineeringClas.ClassName}, classUnit:${softEngineeringClas.classUnit}, ClassCapacity:${softEngineeringClas.ClassCapacity} }` )
-  console.log(`{ClassName :${networkClas.ClassName}, classUnit:${networkClas.classUnit}, ClassCapacity:${networkClas.ClassCapacity}} ` )
+ const networkClass1 = new Person("network", "3", 30,"mmmm");
+ console.log(`{ClassName :${networkClass1.ClassName}, classUnit:${networkClass1.classUnit}, ClassCapacity:${networkClass1.ClassCapacity}  }`)
+ const softEngineeringClass1 = new Person("network", "4", 30,"true");
+ console.log(`{ClassName :${softEngineeringClass1.ClassName}, classUnit:${softEngineeringClass1.classUnit}, ClassCapacity:${softEngineeringClass1.ClassCapacity}  }` );
+
+ softEngineeringClass1.addMyproj();
+ console.log(`{ClassName :${ softEngineeringClass1.ClassName}, classUnit:${ softEngineeringClass1.classUnit}, ClassCapacity:${ softEngineeringClass1.ClassCapacity} ,project:${ softEngineeringClass1.proj} }`);
+ networkClass1.addMybook();
+ console.log(`{ClassName :${networkClass1.ClassName}, classUnit:${networkClass1.classUnit}, ClassCapacity:${networkClass1.ClassCapacity} ,book:${networkClass1.book} }`);
  
  
