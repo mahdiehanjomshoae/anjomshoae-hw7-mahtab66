d540e431156b8185433fee8368e443449ae01f27
mahdieh <mahdieh.anjomshoae98@gmail.com>
2021-12-24 17:01:10 +0330

    omplete

--- Question1/q1.js
+++ Question1/q1.js
@@ -8,16 +8,15 @@ const Person = function(ClassName, classUnit, ClassCapacity,book,proj){
      this.addMyproj = function(){
        this.proj = proj; 
        }
-     
 }
- const networkClass1 = new Person("network", "3", 30,"mmmm");
- console.log(`{ClassName :${networkClass1.ClassName}, classUnit:${networkClass1.classUnit}, ClassCapacity:${networkClass1.ClassCapacity}  }`)
- const softEngineeringClass1 = new Person("network", "4", 30,"true");
- console.log(`{ClassName :${softEngineeringClass1.ClassName}, classUnit:${softEngineeringClass1.classUnit}, ClassCapacity:${softEngineeringClass1.ClassCapacity}  }` );
-
- softEngineeringClass1.addMyproj();
- console.log(`{ClassName :${ softEngineeringClass1.ClassName}, classUnit:${ softEngineeringClass1.classUnit}, ClassCapacity:${ softEngineeringClass1.ClassCapacity} ,project:${ softEngineeringClass1.proj} }`);
+ const networkClass1 = new Person("network", "3", 30,"mmm","mypro");
  networkClass1.addMybook();
  console.log(`{ClassName :${networkClass1.ClassName}, classUnit:${networkClass1.classUnit}, ClassCapacity:${networkClass1.ClassCapacity} ,book:${networkClass1.book} }`);
+ console.log(networkClass1)
+ 
+ const softEngineeringClass1 = new Person("network", "4", 30,"mmmm","mypro");
+ softEngineeringClass1.addMyproj();
+ console.log(`{ClassName :${ softEngineeringClass1.ClassName}, classUnit:${ softEngineeringClass1.classUnit}, ClassCapacity:${ softEngineeringClass1.ClassCapacity} ,project:${ softEngineeringClass1.proj} }`);
+ console.log(softEngineeringClass1)
  
  
