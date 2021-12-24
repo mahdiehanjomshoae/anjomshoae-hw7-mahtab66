2b967486541703741130f07b357ffa113080a800
mahdieh <mahdieh.anjomshoae98@gmail.com>
2021-12-24 21:16:05 +0330

    add complete q1

--- Question1/q1.js
+++ Question1/q1.js
@@ -10,13 +10,14 @@ const Person = function(ClassName, classUnit, ClassCapacity,book,proj){
        }
 }
  const networkClass1 = new Person("network", "3", 30,"mmm","mypro");
+
+ console.log(JSON.stringify(networkClass1));
  networkClass1.addMybook();
- console.log(`{ClassName :${networkClass1.ClassName}, classUnit:${networkClass1.classUnit}, ClassCapacity:${networkClass1.ClassCapacity} ,book:${networkClass1.book} }`);
- console.log(networkClass1)
+ console.log(JSON.stringify(networkClass1) )
  
  const softEngineeringClass1 = new Person("network", "4", 30,"mmmm","mypro");
+ console.log(JSON.stringify(softEngineeringClass1));
  softEngineeringClass1.addMyproj();
- console.log(`{ClassName :${ softEngineeringClass1.ClassName}, classUnit:${ softEngineeringClass1.classUnit}, ClassCapacity:${ softEngineeringClass1.ClassCapacity} ,project:${ softEngineeringClass1.proj} }`);
- console.log(softEngineeringClass1)
+ console.log(JSON.stringify(softEngineeringClass1))
  
  
